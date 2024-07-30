6b9af0dee3bb6b387c6a5325043e0fe639c246b6
Baran Semih Güzel <baransemih.2101@gmail.com>
2024-07-30 21:39:16 +0300

    f

--- App/App.Product/wwwroot/JavaScript.js
+++ App/App.Product/wwwroot/JavaScript.js
@@ -1,26 +1,7 @@
-﻿//function Get() {
-//    var id = document.getElementById("id").value;
-//    $.ajax({
-//        url: "https://localhost:44373/api/Products/" + id,
-//        type: "GET",
-//        contentType: "application/json",
-//        success: function (result) {
-//            document.getElementById("name").value = result.name;
-//            document.getElementById("price").value = result.price;
-//            document.getElementById("category").value = result.category;
-//        },
-//        error: function (xhr, status, error) {
-//            alert(xhr.responseText);
-//        }
-//    });
-
-//}
-
-
-var Form = document.getElementById("formPost");
-Form.addEventListener('submit', async  (e) => {
+﻿var form = document.getElementById("formPost");
+form.addEventListener('submit', async  (e) => {
     e.preventDefault();
-    const formData = new FormData(Form);
+    const formData = new FormData(form);
     const data = {};
     formData.forEach((value, key) => {
         data[key] = value;
